132e2fd0cfe4430065097c808000a218bafb4f25
Gina Sarfaty <gsarfaty@usaid.gov>
2023-01-30 06:28:06 -0500

    update to ADAPTs google sheet

--- Scripts/COVID_Monthly_Processing_IRv5.3.r
+++ Scripts/COVID_Monthly_Processing_IRv5.3.r
@@ -16,15 +16,15 @@ library(glamr)
 # devtools::install_github("USAID-OHA-SI/gagglr")
 
 # SET CREDENTIALS & GLOBALS
-myuser<-("")
-set_email("")
+myuser<-()
+set_email()
 load_secrets()
 #after this step copy and paste into the R profile script and save
 #now stored so don't have to authenticate
 drive_auth()
 gs4_auth()
 
-current_mo_full<-"2022-10-31" #change each month
+current_mo_full<-"2022-12-31" #change each month
 
 
 # READ IN HISTORIC DATA
@@ -44,60 +44,71 @@ GETF_NEXTMILE<-read_sheet(as_sheets_id('1WHdEzsyelf20N60JoP2ascrKQynDvaOEnfV3SRV
                           col_types="c") 
 
   
-ADAPT_RTC <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet = "RTC_v3",
+ADAPT_RTC <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet = "RTC_v3",
                        col_types="c") %>% 
   rename(Prime=Partner) %>% 
   mutate(Sub="Right to Care") %>% 
   relocate(Sub,.after=Prime)
 
 
-ADAPT_Aurum <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet = "Aurum_v3",
+ADAPT_Aurum <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet = "Aurum_v3",
                          col_types="c") %>% 
   rename(Sub=Partner)
 
-ADAPT_FPD <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet="FPD_v3",
+ADAPT_FPD <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="FPD_v3",
                        col_types="c") %>% 
   rename(Sub=Partner)
 
-ADAPT_Genesis <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet="Genesis_v3",
+ADAPT_Genesis <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="Genesis_v3",
                            col_types="c") %>% 
   rename(Sub=Partner)
 
 
-ADAPT_Intrahealth <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet="IntraHealth_v3",
+ADAPT_Intrahealth <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="IntraHealth_v3",
                         col_types="c") %>% 
   rename(Sub=Partner)
 
 
-ADAPT_ReAction <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet="ReAction!_v3",
+ADAPT_ReAction <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="ReAction!_v3",
                             col_types="c") %>% 
   rename(Sub=Partner)
 
 
-ADAPT_Anova <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet="ANOVA_v3",
+ADAPT_Anova <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="ANOVA_v3",
                             col_types="c") %>% 
   rename(Sub=Partner)
 
 
-ADAPT_PulseHealth <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet="PulseHealth_v3",
+ADAPT_PulseHealth <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="PulseHealth_v3",
                          col_types="c") %>% 
   rename(Sub=Partner)
 
 
-ADAPT_WDED <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet="WDED_v3",
+ADAPT_WDED <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="WDED_v3",
                                col_types="c") %>% 
   rename(Sub=Partner)
 
 
-ADAPT_KI <-read_sheet(as_sheets_id('1k2yDUZk49SRjTq3ceeXzogUw1TnMszjdXGtYJFwW060'), sheet="Khethimpilo_v3",
+ADAPT_KI <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="Khethimpilo_v3",
                         col_types="c") %>% 
   rename(Sub=Partner)
+
+ADAPT_HST <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="HST_v3",
+                      col_types="c") %>% 
+  rename(Sub=Partner)
+
+ADAPT_THINK <-read_sheet(as_sheets_id('1v-5xHU9aK59miudKSyY8WLQRZgpaCEZMk-42GXnGWJQ'), sheet="THINK_v3",
+                          col_types="c") %>% 
+  rename(Sub=Partner)
+
+
+
 
 
 ADAPT <-bind_rows(ADAPT_RTC,ADAPT_Aurum, ADAPT_FPD,
                   ADAPT_Genesis,ADAPT_Intrahealth,ADAPT_ReAction,
                   ADAPT_Anova,ADAPT_PulseHealth, ADAPT_WDED,
-                  ADAPT_KI)%>%  
+                  ADAPT_KI, ADAPT_HST,ADAPT_THINK)%>%  
   rename(Partner=Prime) %>% 
   select(Partner,Sub,everything()) %>% 
   mutate(Partner="Right to Care")
@@ -232,7 +243,7 @@ arpa_combined<-arpa %>%
 ind<-distinct(arpa_combined,historic_indicator_code,indicator,numeratordenom)
 
 # EXPORT
-filename<-paste(current_mo_full,"Data_USAID_ARPA_GVAX_COMBINED_v1.3.csv",sep="_")
+filename<-paste(current_mo_full,"Data_USAID_ARPA_GVAX_COMBINED_v2.0.csv",sep="_")
 
 
 write_csv(arpa_combined, file.path(here("Dataout"),filename),na="")
